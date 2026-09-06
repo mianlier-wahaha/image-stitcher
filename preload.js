592db5339dfd93a8ec64feb2d5c547cd144959e2
mianlier-wahaha <58820672+mianlier-wahaha@users.noreply.github.com>
2026-09-06 19:28:38 +0800

    chore: remove preload.js (Electron removed)

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// 只暴露最小、明确的能力给渲染进程
-contextBridge.exposeInMainWorld("stitchApi", {
-  exportImage: (data, ext) =>
-    ipcRenderer.invoke("export-image", { data, ext })
-});
